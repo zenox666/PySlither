41a1ca25ec4899eeaaf24249b1d231258d4f39e1
Allu Ketan Satya Sai <allusai2007@gmail.com>
2025-12-22 02:36:58 +0530

    Implement basic Snake game functionality

---
+++ snake.py
@@ -0,0 +1,45 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+DISTANCE = 20
+UP=90
+DOWN=270
+RIGHT=0
+LEFT=180
+
+class Snake:
+    def __init__(self):
+        self.parts = []
+        self.create_snake()
+        self.head = self.parts[0]
+
+    def create_snake(self):
+        for i in STARTING_POSITIONS:
+            new_part = Turtle("square")
+            new_part.color("white")
+            new_part.penup()
+            new_part.goto(i)
+            self.parts.append(new_part)
+
+    def move(self):
+        for part_num in range(len(self.parts) - 1, 0, -1):
+            x = self.parts[part_num - 1].xcor()
+            y = self.parts[part_num - 1].ycor()
+            self.parts[part_num].goto(x, y)
+        self.head.forward(DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
