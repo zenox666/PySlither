37d6d8493a7c7993b1d6864a8c6a200727997a2e
Allu Ketan Satya Sai <allusai2007@gmail.com>
2025-12-23 23:19:26 +0530

    Integrate food and score management in main.py
    Added food and score functionality to the game.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from score import Score
 import time
 
 
@@ -10,6 +12,8 @@ screen.title("")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+score = Score()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -25,9 +29,20 @@ is_game_on = True
 while is_game_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
+    if snake.head.distance(food) < 18:
+        snake.increase_parts()
+        food.refresh()
+        score.increase_score()
 
+    if snake.head.xcor()>280 or snake.head.xcor()<-300 or snake.head.ycor()>270 or snake.head.ycor()<-290:
+        is_game_on = False
+        score.game_over()
+
+    for part in snake.parts[1:]:
+        if snake.head.distance(part) < 10:
+            is_game_on = False
+            score.game_over()
 
 screen.exitonclick()
