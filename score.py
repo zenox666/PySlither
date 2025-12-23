98e75241f807686d0221be3f1e911cbda6f7b26c
Allu Ketan Satya Sai <allusai2007@gmail.com>
2025-12-23 23:18:34 +0530

    Implement Score class for game scoring
    Added Score class to manage game scoring.

--- score.py
+++ score.py
@@ -1,1 +1,28 @@
+from time import sleep
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Arial",20,"bold")
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+
+        self.score = 0
+        self.penup()
+        self.color("white")
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_score()
+
+    def update_score(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "bold"))
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
 
